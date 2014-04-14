134a52688d07d7f5c1bdb7ae5d172b3eba1af48a
Everton J. Carpes <everton.carpes@gmail.com>
2014-04-14 00:38:15 -0300

    Add some comment validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment
   field :user, type: String
   field :text, type: String
   field :video_id, type: Integer
+
+  validates :video_id, presence: true
+  validates :user,     presence: true
+  validates :text,     presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe :validations do
+    before { subject.valid? }
+
+    it { expect(subject.errors[:video_id]).to include "can't be blank" }
+    it { expect(subject.errors[:user    ]).to include "can't be blank" }
+    it { expect(subject.errors[:text    ]).to include "can't be blank" }
+  end
 end
